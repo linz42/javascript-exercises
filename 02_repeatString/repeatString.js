1f9694e919ad0b6a5bb460e0778c9cba9d885171
linz42 <muzikismylife13@gmail.com>
2022-07-05 11:26:41 -0700

    completed exercise 2 part 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    reply = '';
+    if (number < 0) {
+        return "ERROR";
+    }
+    while (number > 0, number--) {
+        reply = reply.concat(string);
+    }
+    return reply;
 };
 
 // Do not edit below this line
