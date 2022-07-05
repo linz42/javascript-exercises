018929940eb98b73eb100a1c0f3e053dc3e460a8
linz42 <muzikismylife13@gmail.com>
2022-07-05 16:38:23 -0700

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringLetters = [...string];
+    const returnLetters = [];
+    for (let i = 0; i < stringLetters.length; i++) {
+        returnLetters.unshift(stringLetters[i]);
+    }
+    return returnLetters.join("");
 };
 
 // Do not edit below this line
