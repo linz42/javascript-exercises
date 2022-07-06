ea67353e69d8cfe6301522e1e1a9f358dc451835
linz42 <muzikismylife13@gmail.com>
2022-07-05 17:00:23 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    } else if ((typeof a) !== "number" || (typeof b) !== "number") {
+        return 'ERROR';
+    } else if (a < b) {
+        while (a <= b) {
+            sum += a;
+            a++;
+        }
+        return sum;
+    } else if (b < a) {
+        while (b <= a) {
+            sum += b;
+            b++;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
